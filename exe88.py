cdb62ceef3fa087ffc625afa316a0c46e829b795
sangeethagopi <50619394+sangeethagopi@users.noreply.github.com>
2019-07-12 10:03:22 +0530

    Update exe88.py

--- exe88.py
+++ exe88.py
@@ -1,11 +1,7 @@
-#include<stdio.h>
-int main()
-{
-    int n,k,a[100],i,sum=0;
-    scanf("%d\t%d",&n,&k);
-    for(i=0;i<n;i++)
-        scanf("%d",&a[i]);
-    for(i=0;i<k;i++)
-        sum=sum+k[i];
-    printf("%d",sum);
-}
+a,b=input().split()
+b=int(b)
+l=list(map(int,input().split())) 
+sum=0
+for i in range(0,b):
+  sum+=l[i]
+print(sum)
